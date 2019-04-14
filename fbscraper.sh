9b6f8623a6bb639138e7258de1f6cb1371361ca1
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2019-04-14 19:36:53 +0200

    This scrapes the swp.de facebook account instead of swp.de, and uses the poster's comments left there instead of the destination URL title for the tweet's text Obviously, this is a work in progress, as it neither logs URLs to avoid duplicate tweets, nor does it actually tweet the text. Also, it does not yet check if the tweet length is <= 280 chars.

---
+++ fbscraper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PAGEDUMP=$(elinks --dump https://m.facebook.com/swp.de/?locale2=de_DE)
+FBURLS=$(echo -e "$(echo "$PAGEDUMP" | grep lm.facebook.com | grep utm | sed -e 's/^.*u=//g' -e 's/%3Futm_medium.*$//' -e 's/%/\\x/g')" | uniq)
+
+for FBURL in $FBURLS; do
+	ARTICLEID=$(echo -e "$FBURL" | sed -e 's/^.*-\([0-9]*\)\.html$/\1/')
+	PREVIOUSLINKNUMBER=$(echo -e "$PAGEDUMP" | grep $ARTICLEID | tail -n 1 | tr -d ' ' | awk -F '.' '{ print "\\[" $1-2 "\\]" }')
+	LINKNUMBER=$(echo -e "$PAGEDUMP" | grep $ARTICLEID | tail -n 1 | tr -d ' ' | awk -F '.' '{ print "\\[" $1 "\\]" }')
+#echo "ARTICLEID '$ARTICLEID'"
+#echo "PREVIOUSLINKNUMBER '$PREVIOUSLINKNUMBER'"
+#echo "LINKNUMBER '$LINKNUMBER'"
+	#COMMENT=$(echo -e "$PAGEDUMP" | tr '\n' '#' | sed -e 's/^.*\['$PREVIOUSLINKNUMBER'\]/['$PREVIOUSLINKNUMBER']/' -e 's/\['$LINKNUMBER'\].*$//' -e 's/^.*swp.de## *//' -e 's/## */#/' | tr '#' '\n')
+	COMMENT=$(echo -e "$PAGEDUMP" | tr '\n' '#' | sed -e 's/^.*'$PREVIOUSLINKNUMBER'/'$PREVIOUSLINKNUMBER'/' -e 's/'$LINKNUMBER'.*$//' -e 's/^.*swp.de## *//' -e 's/## */#/' -e 's/#*$//' | tr -s '#' ' ')
+	# TODO check comment length
+	TWEETSTRING="$COMMENT $FBURL"
+	echo -e "$TWEETSTRING"
+done
