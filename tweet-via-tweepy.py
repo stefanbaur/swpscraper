27660d8b6724e40915d94d55694695d7b7a76d18
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2023-08-27 14:56:48 +0200

    added parameter to wait on rate limit (i.e. tweepy will retry automatically)

--- tweet-via-tweepy.py
+++ tweet-via-tweepy.py
@@ -8,7 +8,8 @@ with open("../tweepy_credentials.json") as cred_file:
 try:
     client = tweepy.Client(
         consumer_key=credentials["consumer_key"], consumer_secret=credentials["consumer_secret"],
-        access_token=credentials["access_token"], access_token_secret=credentials["access_token_secret"]
+        access_token=credentials["access_token"], access_token_secret=credentials["access_token_secret"],
+        wait_on_rate_limit=True
     )
 
     response = client.create_tweet(
