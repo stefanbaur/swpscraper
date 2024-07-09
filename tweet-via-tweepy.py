d6e8cef184de81211b06087b5d9f9506291ebdd2
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2024-07-09 11:22:02 +0200

    swpscraper does its own rate limit check

--- tweet-via-tweepy.py
+++ tweet-via-tweepy.py
@@ -9,7 +9,8 @@ try:
     client = tweepy.Client(
         consumer_key=credentials["consumer_key"], consumer_secret=credentials["consumer_secret"],
         access_token=credentials["access_token"], access_token_secret=credentials["access_token_secret"],
-        wait_on_rate_limit=True
+        wait_on_rate_limit=False
+        #wait_on_rate_limit=True
     )
 
     response = client.create_tweet(
